8458e8aa6b59d72ae2215c7770ede0a5d3b3c7c4
hemel <smhemel.eu@gmail.com>
2024-06-20 16:37:35 +0600

    Added User component and functionality.

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[0];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+}
