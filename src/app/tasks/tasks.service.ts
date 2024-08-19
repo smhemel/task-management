f5936a56cedbc0f660cf40ff3fcb79ce284165ca
hemel <smhemel.eu@gmail.com>
2024-08-20 00:09:15 +0600

    Added some functionality of tasks service.

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -3,6 +3,14 @@ import { DUMMY_TASKS } from '../dummy-tasks';
 export class TasksService {
   tasks = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((t) => t.userId === userId);
   }
@@ -18,9 +26,16 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.dueDate,
     });
+
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((t) => t.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
