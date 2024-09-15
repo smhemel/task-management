6663a1b3a12686e9494468ce9d3da5e898eea883
hemel <smhemel.eu@gmail.com>
2024-09-16 00:19:23 +0600

    Added user-tasks component.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {}
