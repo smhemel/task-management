012f68adaf52b180cddab52f20b7d5eb2bf0aa63
hemel <smhemel.eu@gmail.com>
2024-09-15 18:09:47 +0600

    Update app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
-import { TasksService } from './tasks/tasks.service';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
-  providers: [TasksService],
+  imports: [HeaderComponent, UsersComponent, RouterOutlet],
 })
-export class AppComponent {
-  users = DUMMY_USERS;
-  selectedUserId?: string;
-
-  get selectedUser() {
-    return this.users.find((u) => u.id === this.selectedUserId);
-  }
-
-  onSelectUser(id: any) {
-    this.selectedUserId = id;
-  }
-}
+export class AppComponent {}
